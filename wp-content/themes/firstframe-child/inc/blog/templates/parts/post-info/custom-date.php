c0fb13f648fa04b046b5f2efa743d8eb2375f646
khottavong <sean.khottavong@gmail.com>
2023-10-31 10:27:55 -0400

    Testing again

--- wp-content/themes/firstframe-child/inc/blog/templates/parts/post-info/custom-date.php
+++ wp-content/themes/firstframe-child/inc/blog/templates/parts/post-info/custom-date.php
@@ -1,10 +1,13 @@
 <?php
 $date_link = empty( get_the_title() ) && ! is_single() ? get_the_permalink() : get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) );
 $classes   = '';
+$day = 'd';
+$month = 'M';
 if ( is_single() || is_page() || is_archive() ) { // This check is to prevent classes for Gutenberg block
 	$classes = 'published updated';
 }
 ?>
-<a itemprop="dateCreated" href="<?php echo esc_url( $date_link ); ?>" class="entry-date <?php echo esc_attr( $classes ); ?>">
-	<?php the_time( get_option( 'date_format' ) ); ?>
-</a><div class="qodef-info-separator-end testing"></div>
+<a itemprop="dateCreated" href="<?php echo esc_url( $date_link ); ?>" class="testing entry-date <?php echo esc_attr( $classes ); ?>">
+    <span class="qodef-time-day"><?php the_time($day); ?></span>
+    <span class="qodef-time-month"><?php the_time($month); ?></span>
+</a>
